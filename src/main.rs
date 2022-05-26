55d2a7308113d934d102cc788a500bd5ad2c6962
Ethan Cox <70488531+EthanC2@users.noreply.github.com>
2022-05-26 16:40:20 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,8 @@ use smc::ScrabbleMoveCalculator;
 
 
 fn main() {
-
+    let calc = ScrabbleMoveCalculator::load_from_file("twl06.txt");
+    for (word, points) in calc.generate_plays("dummyhand") {
+        println!("{}: {}", word, points);  
+    }
 }
